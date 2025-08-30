73011dc64f808e8240e4a779c4a9d15c30a7ef82
Luca Kingler <152486882+JanLucaKingler@users.noreply.github.com>
2025-08-30 13:58:36 +0200

    Create script.js

---
+++ My First Project(JavaScript)/task_2/script.js
@@ -0,0 +1,9 @@
+console.log("Earned amount:");
+console.log("Bubblegum: $202");
+console.log("Toffee: $118");
+console.log("Ice cream: $2250");
+console.log("Milk chocolate: $1680");
+console.log("Doughnut: $1075");
+console.log("Pancake: $80");
+
+console.log("Income: $5405");
